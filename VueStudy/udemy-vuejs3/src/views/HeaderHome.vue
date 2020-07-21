7f332c96e933765198f7efc51164fedb111ecbd7
kota36 <trombone_36_k@yahoo.co.jp>
2020-07-21 21:36:49 +0900

    complete vue

---
+++ VueStudy/udemy-vuejs3/src/views/HeaderHome.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav>
+      <h2>Home</h2>
+      <router-link to="/" active-class="link--active" exact class="link">Home</router-link>
+      <router-link to="/users" active-class="link--active" exact class="link">Users</router-link>
+      <button @click="increment(2)">+1</button>
+      <button @click="decrement(2)">-1</button>
+    </nav>
+</template>
+
+<script>
+export default {
+  methods: {
+    increment() {
+      this.$store.dispatch("increment, 2;")
+    },
+    decrement() {
+      this.$store.dispatch("decrement, 2;")
+    }
+  }
+};
+</script>
+  
+<style scoped>
+  .link {
+    margin-right: 10px;
+  }
+  .link--active { 
+    font-size: 20px;
+  }
+</style>  
